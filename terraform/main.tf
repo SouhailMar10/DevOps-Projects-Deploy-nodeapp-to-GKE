0fef44648bb55c4652c85d65bf9d2c8cbce197be
souhailmarnaoui <marnaouisouh@gmail.com>
2025-07-31 16:26:19 -0500

    Reduce GKE node count to 1 to avoid quota issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,7 +43,7 @@ data "google_client_config" "current" {}
 resource "google_container_cluster" "default" {
   name               = "my-first-cluster"
   location           = "us-central1" # ✅ Match region
-  initial_node_count = 3
+  initial_node_count = 1
   min_master_version = data.google_container_engine_versions.default.latest_master_version
 
   node_config {
